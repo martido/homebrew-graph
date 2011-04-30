ec6927b3037ee6d989a94f67c2127d4d72962f2d
Martin Dobmeier <martin.dobmeier@gmail.com>
2011-04-30 10:47:55 +0200

    Removed trailing whitespace

--- brew-graph.rb
+++ brew-graph.rb
@@ -6,82 +6,82 @@ class BrewGraph
   def initialize(arguments)
     @arguments = arguments
   end
-  
+
   def run
     options = parse_options(@arguments)
-    
+
     data = case options.graph
-      when :installed then 
+      when :installed then
         deps_all.keep_if do |formula, deps|
           installed.include?(formula)
         end
       when :all then deps_all
       end
-    
+
     graph = case options.format
       when :dot then Dot.new.to_graph(data)
       when :graphml then GraphML.new.to_graph(data)
-      end      
-      
+      end
+
     if options.output
       File.open(options.output, 'w') { |file| file.write(graph) }
     else
       puts graph
     end
   end
-  
+
   private
-  
+
     def parse_options(arguments)
       options = default_options
-      
+
       opts = OptionParser.new do |opts|
-      
+
         opts.on('-h', '--help') do
           puts opts
           exit
         end
-        
-        opts.on('-g', '--graph OPTION', [:installed, :all], 
+
+        opts.on('-g', '--graph OPTION', [:installed, :all],
                 'Create graph for <installed|all> Homebrew formulae',
                 'Default: installed') do |g|
           options.graph = g
         end
-        
+
         opts.on('-f', '--format FORMAT', [:dot, :graphml],
-                'Specify FORMAT of graph (dot, graphml)', 
+                'Specify FORMAT of graph (dot, graphml)',
                 'Default: dot') do |f|
           options.format = f
         end
-        
-        opts.on('-o', '--output FILE', 
+
+        opts.on('-o', '--output FILE',
                 'Write output to FILE instead of stdout') do |o|
           options.output = o
         end
       end
-      
+
       begin
         opts.parse!(arguments)
-      rescue OptionParser::InvalidOption, 
-             OptionParser::InvalidArgument, 
+      rescue OptionParser::InvalidOption,
+             OptionParser::InvalidArgument,
              OptionParser::MissingArgument => e
         abort "#{e.message.capitalize}\n#{opts}"
       end
-      
+
       options
     end
-    
+
     def default_options
       opts = OpenStruct.new
       opts.graph = :installed;
       opts.format = :dot;
       opts
-    end    
-    
+    end
+
     def installed
       @installed ||= %x[brew list].split("\n")
-    end    
-    
+    end
+
     def deps_all
       data = {}
       all = %x[brew deps --all].split("\n")
@@ -109,13 +109,13 @@ class Dot
     dot << '}'
     dot.join("\n")
   end
-  
+
   private
-  
+
     def create_node(node)
       %Q(  "#{node}";)
     end
-    
+
     def create_edge(source, target)
       %Q(  "#{source}" -> "#{target}";)
     end
@@ -137,27 +137,27 @@ class GraphML
     end
     out << '  </graph>'
     out << '</graphml>'
-    out.join("\n")    
+    out.join("\n")
   end
-  
+
   private
-  
+
     def header
 <<-EOS
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<graphml 
-  xmlns="http://graphml.graphdrawing.org/xmlns" 
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-  xmlns:y="http://www.yworks.com/xml/graphml" 
-  xmlns:yed="http://www.yworks.com/xml/yed/3" 
+<graphml
+  xmlns="http://graphml.graphdrawing.org/xmlns"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns:y="http://www.yworks.com/xml/graphml"
+  xmlns:yed="http://www.yworks.com/xml/yed/3"
   xsi:schemaLocation="
-    http://graphml.graphdrawing.org/xmlns 
-    http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd">    
+    http://graphml.graphdrawing.org/xmlns
+    http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd">
   <key for="node" id="d0" yfiles.type="nodegraphics"/>
   <key for="edge" id="d1" yfiles.type="edgegraphics"/>
 EOS
     end
-  
+
     def create_node(node)
 <<-EOS
     <node id="#{node}">
@@ -170,7 +170,7 @@ EOS
     </node>
 EOS
     end
-    
+
     def create_edge(source, target)
       @edge_id ||= 0
       @edge_id += 1
